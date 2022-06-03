f0efa72992edac0d6bdf8dd3ab0baac43e88e33d
aroenvr <aroenvr@gmail.com>
2022-06-03 19:29:15 +0200

    first commit

---
+++ src/main/java/com/template/springapptemplate/SpringAppTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.template.springapptemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppTemplateApplication.class, args);
+    }
+
+}
